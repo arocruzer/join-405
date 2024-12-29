0c2530302d5056ae4cacd8a3f267e7474b1a0867
Mihael0599 <mihaeldraz10@gmail.com>
2024-12-29 14:43:56 +0100

    added container with contacts and styles and addUserToTask needs to be changed has small bugs

--- JS/addTask.js
+++ JS/addTask.js
@@ -1,10 +1,30 @@
-let choseUser = document.getElementById ("browsers");
+let dropDownArrow = document.getElementById("drop-down-arrow");
+let concatList = document.getElementById("contact-list");
+let state = 1
+
+function openDropDownMenu() {
+  switch (state) {
+    case 1: 
+      concatList.style.display = 'block'; 
+      dropDownArrow.src = "../Assets/arrow_drop_downaa.png"
+      state = 2; 
+      break;
+    case 2: 
+      concatList.style.display = 'none';
+      dropDownArrow.src = "../Assets/arrow_drop_downaa (1).png"
+      state = 1; 
+      break;
+  }
+}
 
 function addUserToTask() {
-    let options = "";
     users.forEach((user) => {
-        options += `<option value="${user.name}"></option>`;
+        concatList.innerHTML += `<div class="contacts> 
+                          <div class"name-and-img>
+                            <img src="../Assets/Profile badge.png" 
+                            <p>${user.name}"</p>
+                          </div>
+                          <input type="checkbox">
+                        </div>`;
       });
-    
-      choseUser.innerHTML = options;
     }
