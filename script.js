229943758f04bb54552922be24385911fd6b83f1
arocruzer <teddy.planeten_0l@icloud.com>
2025-01-06 16:20:20 +0100

    baseURL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://join-405-43178-default-rtdb.europe-west1.firebasedatabase.app/users";
+const BASE_URL = "https://join-405-43178-default-rtdb.europe-west1.firebasedatabase.app/users";
 let users = [];
 let email = document.getElementById("email");
 let password = document.getElementById("password");
