2360e7c27dcbb66d4df0bfc0ecd713fc1eaa1a30
arocruzer <teddy.planeten_0l@icloud.com>
2025-01-11 13:58:22 +0100

    legal Documents

--- JS/nav-bar.js
+++ JS/nav-bar.js
@@ -1,9 +1,5 @@
-function loadHTML() {
-
-}
-
 async function loadLegalDocument(page) {
-    const response = await fetch('../HTML/legal-notice.html');
+    const response = await fetch('../HTML/' + page + '.html');
     const htmlContent = await response.text();
 
     document.getElementById("main-content").innerHTML = /*html*/`
